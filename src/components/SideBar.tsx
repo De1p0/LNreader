9bbedd1026921777a905ced3e2698164e6f16f05
delphi <124740881+De1p0@users.noreply.github.com>
2026-02-26 19:21:23 -0500

    Fix workflow

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -34,6 +34,7 @@ export default function Sidebar() {
                             </a>
                         </li>
                     ))}
+
                     <div className="flex-1 flex"></div>
 
 
